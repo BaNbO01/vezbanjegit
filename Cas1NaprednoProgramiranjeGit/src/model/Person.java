4a2a6970e5f7c5bbba9363f797103e46df6a9672
BaNbO <bobantodic23@gmail.com>
2024-02-26 23:12:15 +0100

    Napravljeni getteri,setteri, toString, equals i hashCode kao i konstruktor klase Person

--- Cas1NaprednoProgramiranjeGit/src/model/Person.java
+++ Cas1NaprednoProgramiranjeGit/src/model/Person.java
@@ -1,5 +1,55 @@
 package model;
 
-public class Person {
+import java.util.Objects;
 
+public class Person {
+	private String ime;
+	private String prezime;
+	private int age;
+	public String getIme() {
+		return ime;
+	}
+	public void setIme(String ime) {
+		this.ime = ime;
+	}
+	public String getPrezime() {
+		return prezime;
+	}
+	public void setPrezime(String prezime) {
+		this.prezime = prezime;
+	}
+	public int getAge() {
+		return age;
+	}
+	public void setAge(int age) {
+		this.age = age;
+	}
+	public Person(String ime, String prezime, int age) {
+		this.ime = ime;
+		this.prezime = prezime;
+		this.age = age;
+	}
+	public Person() {
+	}
+	@Override
+	public String toString() {
+		return "Person [ime=" + ime + ", prezime=" + prezime + ", age=" + age + "]";
+	}
+	@Override
+	public int hashCode() {
+		return Objects.hash(age, ime, prezime);
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Person other = (Person) obj;
+		return age == other.age && Objects.equals(ime, other.ime) && Objects.equals(prezime, other.prezime);
+	}
+	
+	
 }
